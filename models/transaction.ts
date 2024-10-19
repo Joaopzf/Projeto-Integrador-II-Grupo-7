b689b74056fc7c20cc2139a0554d101f9c8c95e7
Sophia Franco de Godoy <161141869+sophiagodoy@users.noreply.github.com>
2024-10-19 10:45:02 -0300

    Delete models directory

--- models/transaction.ts
+++
@@ -1,7 +0,0 @@
-export interface Transaction {
-    id: number;
-    user_id: number;
-    amount: number;
-    transaction_type: 'deposit' | 'withdraw';
-    created_at: Date;
-}
--- models/wallets.ts
+++
@@ -1,5 +0,0 @@
-export interface Wallet {
-    id: number;
-    user_id: number;
-    balance: number;
-}

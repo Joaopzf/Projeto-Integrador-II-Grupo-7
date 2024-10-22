4a68e384e35a7d2e72895ad58438c55c8a6ea7fd
Pamorimd <priscilaa.amorin@oulook.com>
2024-10-21 21:31:42 -0300

    Inserção da funcionalidade de retirar fundos funcionando

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+//PARTE 1
+import withdrawRoutes from './routes/withdrawRoutes';
+
+const app = express();
+app.use(express.json()); // Middleware para parsear JSON
+
+
+// Registrar as rotas
+app.use('/api', withdrawRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});

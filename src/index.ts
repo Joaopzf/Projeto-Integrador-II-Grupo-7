2a0f979fc7beab4ac14533558eb041c2d4f9faa5
Alicia <alicia.bp@puccampinas.edu.br>
2024-10-23 08:44:56 -0300

    enviando função addfunds

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+//PARTE 1
+ // Importa as rotas de avaliação
+//PARTE 2
+import addFundsRoutes from './routes/addFundsRoutes';
+
+
+const app = express();
+app.use(express.json()); // Middleware para parsear JSON
+
+// Registrar as rotas
+
+app.use('/api', addFundsRoutes);
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});

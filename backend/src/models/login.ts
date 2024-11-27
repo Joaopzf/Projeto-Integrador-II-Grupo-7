7ad5f85db8ac5efb8ef54b998d260b8c2a9f159f
Alícia Pagotto <160851432+aliciapagotto@users.noreply.github.com>
2024-11-26 21:15:08 -0300

    Update login.ts

--- backend/src/models/login.ts
+++ backend/src/models/login.ts
@@ -1,3 +1,4 @@
+// Define a interface Login, que descreve a estrutura de dados para o login de um usuário
 export interface Login {
     email: string;     // O e-mail do usuário
     password: string;  // A senha do usuário

f3b397cffd55e4494058dffaa231dab10d7093c4
Alícia Pagotto <160851432+aliciapagotto@users.noreply.github.com>
2024-11-26 21:26:51 -0300

    Update addFundsRoutes.ts

--- backend/src/routes/addFundsRoutes.ts
+++ backend/src/routes/addFundsRoutes.ts
@@ -1,13 +1,20 @@
+// Importa as dependencias necessarias do express
 import express, { Request, Response, Router } from 'express';
+// Importa a função addFunds, que vai lidar com a lógica de adicionar fundos
 import { addFunds } from '../services/addFunds'; 
 
+
+// Criando uma instância do Router do Express para definir as rotas
 const router: Router = express.Router();
 
+// Função responsável por lidar com a requisição de adicionar fundos à conta do usuário
 const handleAddFunds = async (req: Request, res: Response): Promise<void> => {
     try {
+        // Desestruturando os dados da requisição
         const { userId, amount, bankDetails } = req.body;
 
         // Verificação de dados obrigatórios
+        // Se faltar algum campo essencial (userId, amount ou bankDetails), retorna um erro 400
         if (!userId || amount === undefined || !bankDetails) {
             res.status(400).json({ error: 'Campos obrigatórios ausentes: userId, amount, ou bankDetails.' });
             return;
@@ -34,17 +41,25 @@ const handleAddFunds = async (req: Request, res: Response): Promise<void> => {
             return;
         }
 
+        // Chama a função addFunds para processar o depósito com os dados fornecidos
         const message = await addFunds(userId, amount, bankDetails);
+
+        // Retorna uma resposta com o status 200 caso o depósito seja processado com sucesso
         res.status(200).json({ message });
     } catch (error) {
+        // Tratamento de erro, se algum erro ocorrer durante o processo
         if (error instanceof Error) {
+            // Se o erro for uma instância da classe Error, retorna o erro com a mensagem
             res.status(500).json({ error: `Erro ao processar o depósito: ${error.message}` });
         } else {
+             // Para outros erros desconhecidos, retorna uma mensagem genérica
             res.status(500).json({ error: 'Erro desconhecido ao processar o depósito.' });
         }
     }
 };
 
+// Definindo a rota POST para a funcionalidade de adicionar fundos
 router.post('/addFunds', handleAddFunds);
 
+// Exportando o roteador para ser utilizado em outros módulos
 export default router;

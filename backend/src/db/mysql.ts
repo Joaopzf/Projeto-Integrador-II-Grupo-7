ef58d758ec8bc787a41857abb8dbbe150c522e69
Alícia Pagotto <160851432+aliciapagotto@users.noreply.github.com>
2024-11-26 21:10:48 -0300

    Comentando mysql.ts

--- backend/src/db/mysql.ts
+++ backend/src/db/mysql.ts
@@ -1,8 +1,10 @@
+// importação de bibliotecas
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+// criação do pool de conexões, que gerencia as conexões com o banco de dados
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
@@ -13,4 +15,5 @@ const pool = mysql.createPool({
   queueLimit: 0,
 });
 
+// exporta o pool de conexões para ser usado em outras partes do código
 export default pool;

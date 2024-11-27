3e8735e1ded2a983b28a4172fe835fd0aa03a24d
Alícia Pagotto <160851432+aliciapagotto@users.noreply.github.com>
2024-11-26 21:18:27 -0300

    Update signUp.ts

--- backend/src/models/signUp.ts
+++ backend/src/models/signUp.ts
@@ -1,12 +1,14 @@
+// Define a interface User, que descreve a estrutura de dados para um usuário
 export interface User {
     id?: number; 
     username: string;
     email: string;
     password: string;
-    date_of_birth?: string | null;
+    date_of_birth?: string | null; // Pode ser null se não for fornecida
     token?: string; // Adicionando o token aqui
 }
 
+// Define a interface CreditCard, que descreve a estrutura de dados para um cartão de crédito
 export interface CreditCard {
     number: string; 
     expiry: string; 
